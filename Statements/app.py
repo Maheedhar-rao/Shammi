8f15d98406dddee1bcdf7a138567e169d66ea7cb
Maheedhar rao Govada <106291633+Maheedhar-rao@users.noreply.github.com>
2025-10-31 12:32:26 -0400

    Update app to use environment variable for port

--- Statements/app.py
+++ Statements/app.py
@@ -199,4 +199,6 @@ def _unauth():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5055, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5055)) 
+    app.run(host="0.0.0.0", port=port, debug=True)
